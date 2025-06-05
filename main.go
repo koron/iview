5285c9a74b8b7b20fb4c2969f4aa1b8b3d94eb32
MURAOKA Taro <koron.kaoriya@gmail.com>
2025-06-05 12:20:55 +0900

    comments

--- main.go
+++ main.go
@@ -17,19 +17,14 @@ func main() {
 		dirpath = "."
 	)
 
+	// Provide static contents under "/_/"
 	contents, err := fs.Sub(assetsFS, "assets")
 	if err != nil {
 		log.Fatal(err)
 	}
 	http.Handle("/_/", http.StripPrefix("/_/", http.FileServer(http.FS(contents))))
 
-	//os.DirFS()
-	//root, err := os.OpenRoot(dirpath)
-	//if err != kkkkkkkk
-	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	//	fmt.Printf("path: %s\n", r.URL.Path)
-	//})
-
+	// Provide dynamic contents
 	http.Handle("/", http.FileServer(http.FS(os.DirFS(dirpath))))
 
 	log.Fatal(http.ListenAndServe(address, nil))
